5edfca8df4067703f79c779a753eb25d5ef4e553
KeyllaCastro <keylla.castro@etec.sp.gov.br>
2024-11-05 20:48:22 -0300

    Exercícios feitos 05/11/2024
    Exercícios feitos 05/11/2024

---
+++ Lista2/3/3.js
@@ -0,0 +1,11 @@
+//Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+sexo = (prompt("Se você se identifica com sexo Feminino digite F, se você se identifica com Masculino digite M: "))
+//Conforme a letra escrever: “F – Feminino”, ou “M – Masculino” ou Sexo Inválido.
+
+if ( sexo == "F") {
+    alert("Você se identifica com F - Feminino");
+}else if (sexo == "M"){
+    alert("Você se identifica com M - Masculino");
+}else {
+    alert("Sexo Inválido");
+}
